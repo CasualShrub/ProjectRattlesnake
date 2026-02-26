6938ee1775bb402799df31a0f26a4fb5fd3195cc
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-02-26 11:34:22 +0800

    Setup JFrame

--- RattleSnake/src/App.java
+++ RattleSnake/src/App.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int screenWidth = 600;
+        int screenHeight = screenWidth;
+
+        JFrame frame = new JFrame("RattleSnake");
+        frame.setVisible(true);
+        frame.setSize(screenWidth, screenHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
