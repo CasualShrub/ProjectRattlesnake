a60fec4d70ddd6f8207953429cd3d0cc68cc787c
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-02-26 14:14:22 +0800

    Move Tile class to its own file

--- RattleSnake/src/Game.java
+++ RattleSnake/src/Game.java
@@ -13,17 +13,6 @@ import java.util.Random;
 import javax.swing.*;
 
 public class Game extends JPanel implements ActionListener, KeyListener {
-
-    private class Tile {
-        int x;
-        int y;
-
-        Tile (int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     int screenWidth;
     int screenHeight;
     int tileSize = 20;
