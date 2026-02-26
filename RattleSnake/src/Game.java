a9a6a97b3c2dd9030246dc60404c4b6f7059ade9
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-02-26 11:49:36 +0800

    Draw Tiles

--- RattleSnake/src/Game.java
+++ RattleSnake/src/Game.java
@@ -5,13 +5,43 @@ import java.util.Random;
 import javax.swing.*;
 
 public class Game extends JPanel{
+
+    private class Tile {
+        int x;
+        int y;
+
+        Tile (int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     int screenWidth;
     int screenHeight;
+    int tileSize = 20;
+
+    Tile snakeHead = new Tile(7, 7);
+
 
     Game(int screenWidth, int screenHeight) {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
         setPreferredSize(new Dimension(this.screenWidth, this.screenHeight));
-        setBackground(Color.cyan);
+        setBackground(Color.black);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        for (int i = 0; i < screenWidth/tileSize; i++){
+            g.drawLine(i*tileSize, 0, i*tileSize, screenHeight); //vertical line
+            g.drawLine(0, i*tileSize, screenWidth, i*tileSize);
+        }
+        
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
     }
 }
